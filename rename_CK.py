9b6f9c7759f6767aca33c22bc996681598114cff
C-KENNY <csk29@uclive.ac.nz>
2014-06-29 12:40:28 +1200

    Added ability to process multiple ()'s and []'s

--- rename_CK.py
+++ rename_CK.py
@@ -1,16 +1,17 @@
 import os, re
 
+def regularFunction(filename):
+    """ Removes leading and trailing white space, as well as contents
+        between () and [], also removes said brackets """
+   
+    newName = filename.strip() # removes leading and trailing white space
+    newName = re.sub(r"\(*([A-Za-z0-9_]+\))*","", newName)   # ()
+    newName = re.sub(r"\[*([A-Za-z0-9_]+\])*","", newName)   # []
+    os.rename(filename, newName)
+
 if __name__ == '__main__':
     for filename in os.listdir("."):
-        """ For each file in current directory. The RE is
-            split into two lines for readability, as it's
-            usually <40 characters to process. """
-
-        newName = re.sub(r"\(([A-Za-z0-9_]+\))","", filename)    
-        newName = re.sub(r"\[([A-Za-z0-9_]+\])","", newName)
-        
-        
-        os.rename(filename, newName)
-
-
-
+        """ For each file in current directory. The RE is split into
+            multiple lines for readability, as it's usually <=40 
+            characters to process. """
+        regularFunction(filename) 
